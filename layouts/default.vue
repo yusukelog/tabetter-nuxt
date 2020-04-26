520e9182b749c51e3a5103d6f9f1504b52f3e1ed
koga yusuke <web@yusukelog.com>
2020-04-27 00:51:45 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app light>
+    <v-app-bar color="primary" dark fixed flat app>
+      <v-toolbar-title class="headline font-weight-bold" v-text="title" />
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" color="primary" dark app>
+      <v-col class="text-center" cols="12">
+        &copy; {{ new Date().getFullYear() }} — <strong>Tabetter</strong>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Tabetter'
+    }
+  }
+}
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-layout>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card>
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>ログイン</v-toolbar-title>
+            <v-spacer />
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn href="source" icon large target="_blank" v-on="on">
+                  <v-icon>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  large
+                  href="https://codepen.io/johnjleider/pen/pMvGQO"
+                  target="_blank"
+                  v-on="on"
+                >
+                  <v-icon>mdi-codepen</v-icon>
+                </v-btn>
+              </template>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="メールアドレス"
+                name="email"
+                prepend-icon="mdi-email-outline"
+                type="email"
+              />
+              <v-text-field
+                label="パスワード"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary">ログイン</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    source: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  }
+}
+</script>
